3b4c5b9303e5b498a407b342078900aa0c40ddd3
Victor Borges <107185408+victrborges@users.noreply.github.com>
2022-07-07 15:02:34 -0300

    Delete view directory

--- view/categoria/form.php
+++
@@ -1,6 +0,0 @@
-<!--Conteúdo da página-->
-<div class="container">
-      <h1 class="mt-2">Cadastro de Categorias</h1>
-      <hr>
-</div>
-    
--- view/template/conteudo.php
+++
@@ -1,6 +0,0 @@
-
-  <!--Conteúdo da página-->
-    <div class="container">
-      <h1 class="mt-2">Conteúdo</h1>
-      <hr>
-    </div>
